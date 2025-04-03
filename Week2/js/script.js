98a2b1a0d1c9419df5dab7421b8a8c5c3181041c
andrewj427 <aajohnston@email.neit.edu>
2025-04-03 09:07:50 -0400

    end of class lab week 2

---
+++ Week2/js/script.js
@@ -0,0 +1,5 @@
+// example of JavaScript file
+
+var name = prompt("What is your name?");
+document.getElementById("greeting").innerHTML = `Hello ${name} how are you today?`
+document.getElementById("greeting").style.color = "red";
